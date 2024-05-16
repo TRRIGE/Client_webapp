b44b6717284efa4f90bc36f244add1dd211cbfeb
Pratik Samarth <pratiksamarth29@gmail.com>
2024-05-16 23:58:15 +0530

    scroller added

--- app/layout.js
+++ app/layout.js
@@ -17,6 +17,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous" async />
+        <script src="./jQuery/jquery.js" async></script>
+        <script src="./jQuery/jquery.scrollUp.min.js" async></script>
       </head>
       <body className={quicksand.className}>
         {children}
